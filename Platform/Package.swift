83557f5b4a3b705890a740c9b2f02dd27ab98274
xloha <povoq1024@gmail.com>
2022-02-11 15:54:52 +0900

    라이브러리 파일 추가 * DefaultsStore * Network * NetworkImp

--- Platform/Package.swift
+++ Platform/Package.swift
@@ -19,6 +19,18 @@ let package = Package(
 			name: "SuperUI",
 			targets: ["SuperUI"]
 		),
+        .library(
+            name: "DefaultsStore",
+            targets: ["DefaultsStore"]
+        ),
+        .library(
+            name: "Network",
+            targets: ["Network"]
+        ),
+        .library(
+            name: "NetworkImp",
+            targets: ["NetworkImp"]
+        ),
     ],
     dependencies: [
 		.package(url: "https://github.com/CombineCommuinty/CombineExt", from: "1.0.0"),
@@ -43,5 +55,23 @@ let package = Package(
 				"RIBsUtil"
 			]
 		),
+        .target(
+            name: "DefaultsStore",
+            dependencies: [
+                
+            ]
+        ),
+        .target(
+            name: "Network",
+            dependencies: [
+                
+            ]
+        ),
+        .target(
+            name: "NetworkImp",
+            dependencies: [
+                "Network"
+            ]
+        ),
     ]
 )
